c2ef76fa3eca30bbfa53dd93ca932fde8062a547
Evelyn Tran <evelyn.t.0108@gmail.com>
2024-09-15 17:20:17 -0500

    backtracking

---
+++ Medium /backtracking/permutation2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        def backtrack(comb, counter):
+            if len(comb) == len(nums):
+                results.append(list(comb))
+                return 
+            for num in counter:
+                if counter[num] > 0:
+                    comb.append(num)
+                    counter[num] -= 1
+                    backtrack(comb, counter)
+                    comb.pop()
+                    counter[num] += 1
+        backtrack([], Counter(nums))
+        return results
+
+            
+
+        
